3f9f7e119f1c7dd5e32d9df543336dcfdcd37626
unknown <lorenzoquaroni24@gmail.com>
2025-07-09 22:17:30 +0200

    esercizi facili

---
+++ NumPari.java
@@ -0,0 +1,11 @@
+public class NumPari {
+    public static void main(String[] args) {
+    //  Stampa tutti i numeri pari in un array di interi.
+        int a[] = {1,2,3,4,5,6,7,8};
+        for(int num : a){
+            if(num%2 == 0){
+                System.out.println(num);
+            }
+        }
+    }
+}
---
+++ SumArray.java
@@ -0,0 +1,11 @@
+public class SumArray {
+        public static void main(String[] args) {
+    //  Somma di tutti i contenuti dell'array.
+        int a[] = {1,2,3};
+        int sum = 0;
+        for(int i = 0; i < a.length ; i++){
+            sum += a[i];
+        }
+        System.out.println(sum);
+    }
+}

de703d2c880b54567760e3237bbb2ba8c646f425
unknown <lorenzoquaroni24@gmail.com>
2025-07-11 08:10:07 +0200

    Duplicati

---
+++ Duplicate.java
@@ -0,0 +1,14 @@
+public class Duplicate {
+    public static void main(String[] args) {
+            int[] arr = {1, 2, 3, 4, 4,4};
+            int count = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}

923f2ce5ffc8caf250b3087245d0f7c4c3651eb4
unknown <lorenzoquaroni24@gmail.com>
2025-07-16 15:18:09 +0200

    Studio approfondito del merge sort, capito

--- ArrayInvertito.java
+++ ArrayInvertito.java
@@ -5,8 +5,8 @@ public class ArrayInvertito {
         int arr[] = {1,2,3,4,5};
         int invertito[] = new int[arr.length];
                 for (int i = 0; i < arr.length; i++) {
-            invertito[i] = arr[arr.length - 1 - i];
+                    invertito[i] = arr[arr.length - 1 - i];
         }
         System.out.println(Arrays.toString(invertito));
-    }    
+    }
 }

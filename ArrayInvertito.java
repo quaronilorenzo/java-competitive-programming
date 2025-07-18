40cd81929ff26f05e610dca12ac3f59e3d8996fb
unknown <lorenzoquaroni24@gmail.com>
2025-07-18 15:55:59 +0200

    Lavoro con matrice

--- ArrayInvertito.java
+++ ArrayInvertito.java
@@ -4,8 +4,8 @@ public class ArrayInvertito {
     public static void main(String[] args){
         int arr[] = {1,2,3,4,5};
         int invertito[] = new int[arr.length];
-                for (int i = 0; i < arr.length; i++) {
-                    invertito[i] = arr[arr.length - 1 - i];
+            for (int i = 0; i < arr.length; i++) {
+                invertito[i] = arr[arr.length - 1 - i];
         }
         System.out.println(Arrays.toString(invertito));
     }

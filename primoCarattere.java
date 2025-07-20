069a9e2febdc2bde97090f2b89ce28df751ccd60
unknown <lorenzoquaroni24@gmail.com>
2025-07-20 09:14:50 +0200

    somma numeri pari array

--- primoCarattere.java
+++ primoCarattere.java
@@ -7,13 +7,13 @@ public class primoCarattere {
         HashMap<Character, Boolean> primo = new HashMap<>(); // creiamo un hashmap dove metteremo ogni carattere con le volte che è ripetuto
         for(int i = 0; i < n; i++){
             char c = parola.charAt(i);
-            if(primo.containsKey(c)){ // se il carattere è nel hasmap il valore della chiave aumenta 
+            if(primo.containsKey(c)){ // se il carattere è nel hasmap il valore della chiave diventa falso
                 primo.put(c, false);
             }else{
                 primo.put(c, true);
             }
         }
-        for(int i = 0;  i < n; i++){
+        for(int i = 0;  i < n; i++){ // se il valore è true vuoldire che c non è ripetuto
             char c = parola.charAt(i);
             if(primo.get(c) == true){
                 System.out.println("Primo carattere non ripetuto: " + c);

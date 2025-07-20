3fd10248b90eda546461b41c226a748e20ccf169
unknown <lorenzoquaroni24@gmail.com>
2025-07-20 16:42:12 +0200

    Indice maggiore di x

---
+++ elementoMag.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class elementoMag {
+    public static void main(String[] args) {
+        // Dato un array ordinato di interi, scrivi un metodo che trovi l indice del primo elemento maggiore di un certo valore
+        int arr[] = {1,2,3,4,5,6,7,8,9,10};
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.print("Enter x:");
+        int x = myObj.nextInt();  // Read user input
+        System.out.println(x);
+        boolean flag = false;
+        for(int i = 0 ; i < arr.length; i++){
+            if(arr[i] > x){
+                System.out.println("Il primo indice del primo elemento maggiore di " + x + ": " + i);
+                flag = true;
+                return;
+            }
+        }
+        if(flag == false){
+            System.out.println("non c'è nessun indice di ogni elemento che è maggiore di x");
+        }
+        
+    }
+}

a81a0049d0c0d085ebcaab405c26786492573e29
unknown <lorenzoquaroni24@gmail.com>
2025-07-21 16:22:23 +0200

    Inserire un valore (no doppioni), rimuoverlo, far uscire un random tra i numeri con probabilità equiprobabile

---
+++ RandomizedSet.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class RandomizedSet {
+    private List<Integer> list;
+    private Map<Integer, Integer> map;
+    private Random rand;
+
+    public RandomizedSet() {
+        this.list = new ArrayList<>();
+        this.map = new HashMap<>();
+        this.rand = new Random();
+    }
+    public boolean insert(int val) {
+        if (map.containsKey(val)) {
+            return false;
+        }
+        list.add(val);
+        map.put(val, list.size() - 1);
+        return true;
+    }
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) {
+            return false;
+        }
+        int index = map.get(val);
+        int lastElement = list.get(list.size() - 1);
+        list.set(index, lastElement);
+        map.put(lastElement, index);
+        list.remove(list.size() - 1);
+        map.remove(val);
+        return true;
+    }
+    public int getRandom() {
+        int randomIndex = rand.nextInt(list.size());
+        return list.get(randomIndex);
+    }
+    public static void main(String[] args) {
+        RandomizedSet set = new RandomizedSet();
+        System.out.println(set.insert(10)); // true
+        System.out.println(set.insert(20)); // true
+        System.out.println(set.insert(10)); // false (già inserito)
+        System.out.println(set.remove(10)); // true
+        System.out.println(set.getRandom()); // 20 (unico elemento rimasto)
+    }
+}

3618536069d9d9e43ef93942ec4e1e290370cfe8
unknown <lorenzoquaroni24@gmail.com>
2025-07-26 08:29:10 +0200

    Formula cofee

---
+++ FormulaCoffee.java
@@ -0,0 +1,24 @@
+public class FormulaCoffee {
+    public static int formula(int h, int coffee) {
+        for (int cups = 1; cups <= 5000; cups++) {
+            h += coffee;
+            String esad = Long.toHexString(h).toUpperCase();
+            if (esad.contains("DEAD")) {
+                return cups;
+            }
+        }
+        return 0;
+    }
+    public static int[] limits(int date) {
+        int cafe = 0xCAFE;
+        int decaf = 0xDECAF;
+        int regularLimit = formula(date, cafe);
+        int decafLimit = formula(date, decaf);
+        return new int[] {regularLimit, decafLimit};
+    }
+    public static void main(String[] args) {
+        int h = 19641128;
+        int[] cupsDeath = limits(h);
+        System.out.println("Limite di caffeinati:" + cupsDeath[0] + " Limite decaffeinati:" + cupsDeath[1]); // [2645, 1162]
+    }
+}
--- TriangoloMatrice.java
+++ TriangoloMatrice.java
@@ -8,8 +8,8 @@ public class TriangoloMatrice {
         System.out.println();
         }
         /* ottimo esercizio per capire bene come lavorare coi for, perchè se noi guardiamo il
-           secondo ciclo for capiamo che stamperà fino alla lunghezza della riga fino ad'ora, 
-           quindi se siamo al primo giro introurremo j = 0, stamperà 1 e POI fa la condizione perchè funziona cosi il cilo for
+        secondo ciclo for capiamo che stamperà fino alla lunghezza della riga fino ad'ora, 
+        quindi se siamo al primo giro introurremo j = 0, stamperà 1 e POI fa la condizione perchè funziona cosi il cilo for
         */
     }
 }

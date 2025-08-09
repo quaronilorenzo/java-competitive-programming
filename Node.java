b4e23b023060a5bae2d11651542c1d912ccd5e52
Quaro <135996757+quaronilorenzo@users.noreply.github.com>
2025-08-09 11:52:03 +0200

    storing at index in a linkedlist

--- Node.java
+++ Node.java
@@ -15,7 +15,6 @@ public class Node {
     private static Node stackingList(int[] arr){
         Node head = new Node(arr[0]);
         Node mover = head; // store l'indirizzo di memoria di Head
-        System.out.println(mover.data); 
         for(int i = 1; i < arr.length;i++){
             Node temp = new Node(arr[i]);
             mover.next =  temp;
@@ -44,9 +43,28 @@ public class Node {
         temp.next = newTail;
         return head;
     }
+    public static Node insertAtIndex(Node head, int index, int val) {
+    // Se index == 0 → insertAtHead
+    // Scorri fino al nodo prima dell'indice
+    // Collega il nuovo nodo in mezzo
+    if(index == 0){
+        insertAtHead(head, val);
+        return head; // se l'indice è 0 insertiamo dal head
+    }
+    int i = 1; // contatore che ci servirà ad arrivare fino al index
+    Node temp = head.next; // head la abbiamo sistemata, ora iniziamo dal prossimo
+    Node newVal = new Node(val); // creiamo il nuovo nodo
+    while(i != (index-1)){ // loopiamo fino al indice come da suggerimento dalla richiesta, 
+        temp = temp.next; // continuiamo a ciclare il nodo
+        i++; // si incrementa l'indice
+    }
+    temp.next = newVal; // se vogliamo storare il nuovo valore dobbiamo fare temp.next non temp e basta, perchè siamo un indice dietro per metterlo nella posizione richiesta
+    System.out.println(temp.next.data); // verifichiamooo
+    return head;
+    }
     public static void main(String[] args) {
         int[] arr = {4,1,2,4,5};
         Node head = stackingList(arr);
-
+        head = insertAtIndex(head, 2, 50);
     }
 }

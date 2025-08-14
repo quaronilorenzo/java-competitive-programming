f0148ef761789dfb7c4ecf9f2f24051b2fc01523
Quaro <135996757+quaronilorenzo@users.noreply.github.com>
2025-08-14 08:45:31 +0200

    modifica bubble sort, sistemato e capito

--- SortingAlgorthms/bubbleSort.java
+++ SortingAlgorthms/bubbleSort.java
@@ -1,14 +1,12 @@
 package SortingAlgorthms;
 
 public class bubbleSort {
-    public static int[] bubbleSortFunction(int[] arr){
+    public static void bubbleSortFunction(int[] arr){
         int temp;
-        int n = arr.length;
-        for(int k = 0; k< n-1; k++) {
-        // (n-k-1) is for ignoring comparisons of elements which have already been compared in earlier iterations
-            for(int i = 0; i < n-k-1; i++) {
+        int len = arr.length;
+        for(int k = 0; k< len-1; k++) {
+            for(int i = 0; i < len-k-1; i++) {
                 if(arr[i] > arr[i+1] ) {
-                    // here swapping of positions is being done.
                     temp = arr[i];
                     arr[i] = arr[i+1];
                     arr[i+1] = temp;
@@ -18,11 +16,10 @@ public class bubbleSort {
         for(int i = 0; i < arr.length; i++){
             System.out.println(arr[i]);
         }
-        return arr;
     }
 
     public static void main(String[] args) {
         int[] arr = {23,45,2,5,6};
-        System.out.println(bubbleSortFunction(arr));
+        bubbleSortFunction(arr);
     }
 }
